ddc593d2bb5d028c7ca150d58d1dadd8f7a037d3
Griffia <griffia@hotmail.co.uk>
2025-06-25 23:27:38 +0100

    Uploading all files
    Uploaded completed project.

---
+++ ExamScriptChanger/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using WinForms = System.Windows.Forms;
+
+namespace ExamScriptChanger
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximise_Click(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void selectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            //dialog.InitialDirectory()
+            dialog.ShowDialog();
+            foreach(string file in  dialog)
+            {
+                Console.WriteLine(file);
+            }
+            
+        }
+
+        private void selectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog();
+        }
+    }
+}
